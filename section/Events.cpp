30ba7e85e098194d48f9ba75bac2ec4c076696bb
RutreD <artur.yes10@gmail.com>
2022-08-20 22:04:07 +0300

    refactor

--- section/Events.cpp
+++ section/Events.cpp
@@ -1,16 +1,15 @@
 #include <cstdint>
 #include "include/LuaAPI.h"
-void LuaRegister(LuaState *LPlus, const char *name, const char *description, lua_CFunction cfunc);
 
-//call patches
+// call patches
 void PatchHeightRangeRings(uintptr_t Sim); // RangeRing.cpp
 
-//register new lua functions
+// register new lua functions
 int SetInvertMiddleMouseButton(lua_State *L); // EngineNewLuaFunctions.cpp
 
 uintptr_t __cdecl OnInitializeConsole()
 {
-    auto LPlus = reinterpret_cast<LuaState*>(0x1104410);
+    auto LPlus = reinterpret_cast<LuaState *>(0x1104410);
     LuaRegister(LPlus, "SetInvertMiddleMouseButton", "SetInvertMiddleMouseButton(true or false)", SetInvertMiddleMouseButton);
 
     return 0x1104410;
@@ -36,21 +35,4 @@ uintptr_t __cdecl OnCreateWldSession(void *a1, void *a2, void *a3, void *a4)
     LuaRegister(LPlus, "SetInvertMiddleMouseButton", "SetInvertMiddleMouseButton(true or false)", SetInvertMiddleMouseButton);
 
     return WldSession;
-}
-
-void LuaRegister(LuaState *LPlus, const char *name, const char *description, lua_CFunction cfunc)
-{
-    struct luaFuncDescReg
-    {
-        uintptr_t RegisterFunc;
-        const char *FuncName;
-        const char *ClassName;
-        const char *FuncDesc;
-        luaFuncDescReg *Next;
-        lua_CFunction FuncPtr;
-        void *ClassPtr;
-    };
-    luaFuncDescReg descReg{0, name, reinterpret_cast<const char *>(0x00E00D90) /*<global>*/, description, 0, cfunc, 0};
-
-    reinterpret_cast<void(__thiscall *)(luaFuncDescReg *, LuaState *)>(0x4CD3A0)(&descReg, LPlus);
 }
