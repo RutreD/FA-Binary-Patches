02a9b060f96c60e77129480f1799637055d19b6a
RutreD <artur.yes10@gmail.com>
2022-09-05 13:06:04 +0300

    fix build --std=c++17

--- section/RangeRings2.cpp
+++ section/RangeRings2.cpp
@@ -2,24 +2,27 @@
 
 float MinCHeight, MaxCHeight;
 
-void* _thiscall HeightCylinders(char* this_)
+uintptr_t __thiscall HeightCylinders(uintptr_t this_)
 {
-    auto Sim = reinterpret_cast<char* (_thiscall *)(void*)>(0x7433B0)(this_);
-    auto STIMap = *(uint32_t*)(Sim + 0x8CC);
-    auto MapData = *(uint32_t*)STIMap;
-    auto Heights = (uint16_t*)*(uint32_t*)MapData;
-    auto SizeX = *(int*)(MapData + 0x4);
-    auto SizeY = *(int*)(MapData + 0x8);
+    auto Sim = reinterpret_cast<uintptr_t(__thiscall *)(uintptr_t)>(0x7433B0)(this_);
+    auto STIMap = *(uint32_t *)(Sim + 0x8CC);
+    auto MapData = *(uint32_t *)STIMap;
+    auto Heights = (uint16_t *)*(uint32_t *)MapData;
+    auto SizeX = *(int *)(MapData + 0x4);
+    auto SizeY = *(int *)(MapData + 0x8);
     uint16_t MinH = -1, MaxH = 0;
-    for (auto I = 0; I < SizeX * SizeY; I++) {
-        if (*Heights < MinH) MinH = *Heights;
-        if (*Heights > MaxH) MaxH = *Heights;
+    for (auto I = 0; I < SizeX * SizeY; I++)
+    {
+        if (*Heights < MinH)
+            MinH = *Heights;
+        if (*Heights > MaxH)
+            MaxH = *Heights;
         Heights++;
     }
     MinCHeight = MinH * 0.0078125f - 5.f;
     MaxCHeight = MaxH * 0.0078125f;
-    this_ = (char*)*(uint32_t*)(0x10C7C28);
-    reinterpret_cast<void (_thiscall *)(void*)>(0x7EDFE0)(this_ + 0x37C);
-    reinterpret_cast<void (_thiscall *)(void*)>(0x81C0C0)(this_ + 0x410);
+    this_ = *(uintptr_t *)(0x10C7C28);
+    reinterpret_cast<void(__thiscall *)(void *)>(0x7EDFE0)(this_ + 0x37C);
+    reinterpret_cast<void(__thiscall *)(void *)>(0x81C0C0)(this_ + 0x410);
     return Sim;
 }
