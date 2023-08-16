624d9a99fdb881bbc035bf9d0b888d3ff69b57a1
RutreD <artur.yes10@gmail.com>
2023-08-16 18:53:19 +0300

    code style

--- section/OnCreateInitLuaState.cpp
+++ section/OnCreateInitLuaState.cpp
@@ -4,35 +4,32 @@ void __thiscall OnCreateInitLuaState(LuaState* state, int enumStdLibs) {
     reinterpret_cast<void(__thiscall*)(LuaState*, int)>(0x90AC10)(state, enumStdLibs); //original
 
     lua_register(state->m_state, "GetProcessAffinityMask", [](lua_State *L) -> int {
-                    //bool, unsigned long, unsigned long = GetProcessAffinityMask()
-                    auto GetCurrentProcess = *reinterpret_cast<void*(__stdcall**)()>(0x00C0F58C);
-                    auto GetProcessAffinityMask = *reinterpret_cast<int(__stdcall**)(void*, unsigned long*, unsigned long*)>(0x00C0F584);
-                    unsigned long dwProcessAffinityMask;
-                    unsigned long dwSystemAffinityMask;
-                    int res = GetProcessAffinityMask(GetCurrentProcess(), &dwProcessAffinityMask, &dwSystemAffinityMask);
-                    lua_pushboolean(L, res != 0);
-                    lua_pushnumber(L, dwProcessAffinityMask);
-                    lua_pushnumber(L, dwSystemAffinityMask);
-                    return 3;
-                });
+        //bool, uint32_t, uint32_t = GetProcessAffinityMask()
+        auto GetProcessAffinityMask = *reinterpret_cast<int(__stdcall**)(void*, uint32_t*, uint32_t*)>(0x00C0F584);
+        uint32_t dwProcessAffinityMask;
+        uint32_t dwSystemAffinityMask;
+        int res = GetProcessAffinityMask(GetCurrentProcess(), &dwProcessAffinityMask, &dwSystemAffinityMask);
+        lua_pushboolean(L, res != 0);
+        lua_pushnumber(L, dwProcessAffinityMask);
+        lua_pushnumber(L, dwSystemAffinityMask);
+        return 3;
+    });
 
     lua_register(state->m_state, "SetProcessAffinityMask", [](lua_State *L) -> int {
-                    //bool SetProcessAffinityMask(unsigned long dwProcessAffinityMask)
-                    unsigned long dwProcessAffinityMask = luaL_checknumber(L, 1);
-                    auto GetCurrentProcess = *reinterpret_cast<void*(__stdcall**)()>(0x00C0F58C);
-                    auto SetProcessAffinityMask = *reinterpret_cast<int(__stdcall**)(void*, unsigned long)>(0x00C0F444);
-                    int res = SetProcessAffinityMask(GetCurrentProcess(), dwProcessAffinityMask);
-                    lua_pushboolean(L, res != 0);
-                    return 1;
-                });
+        //bool SetProcessAffinityMask(uint32_t dwProcessAffinityMask)
+        uint32_t dwProcessAffinityMask = luaL_checknumber(L, 1);
+        auto SetProcessAffinityMask = *reinterpret_cast<int(__stdcall**)(void*, uint32_t)>(0x00C0F444);
+        int res = SetProcessAffinityMask(GetCurrentProcess(), dwProcessAffinityMask);
+        lua_pushboolean(L, res != 0);
+        return 1;
+    });
 
     lua_register(state->m_state, "SetProcessPriority", [](lua_State *L) -> int {
-                    //bool SetProcessPriority(unsigned long dwPriorityClass)
-                    unsigned long dwPriorityClass = luaL_checknumber(L, 1);
-                    auto GetCurrentProcess = *reinterpret_cast<void*(__stdcall**)()>(0x00C0F58C);
-                    auto SetPriorityClass = reinterpret_cast<unsigned long(__stdcall*)(void*, unsigned long)>(GetProcAddress(GetModuleHandle("kernel32.dll"), "SetPriorityClass"));
-                    int res = SetPriorityClass(GetCurrentProcess(), dwPriorityClass);
-                    lua_pushboolean(L, res != 0);
-                    return 1;
-                });
+        //bool SetProcessPriority(uint32_t dwPriorityClass)
+        uint32_t dwPriorityClass = luaL_checknumber(L, 1);
+        auto SetPriorityClass = reinterpret_cast<uint32_t(__stdcall*)(void*, uint32_t)>(GetProcAddress(GetModuleHandle("kernel32.dll"), "SetPriorityClass"));
+        int res = SetPriorityClass(GetCurrentProcess(), dwPriorityClass);
+        lua_pushboolean(L, res != 0);
+        return 1;
+    });
 }
