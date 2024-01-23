2955b90cdd574bfda5592242cd15b2618e3a9a0d
G C <37224614+Hdt80bro@users.noreply.github.com>
2024-01-23 00:05:39 -0800

    Update intelligence every tick (#63)

--- hooks/IntelUpdate.cpp
+++ hooks/IntelUpdate.cpp
@@ -2,21 +2,23 @@
 
 asm(R"(
 
-#Moho::CIntelPosHandle::UpdatePos at 0x76F1E0
-# Intel handles update the intel raster grid when one of the following two
-# conditions are met:
-#   * The unit has moved more than a certain ratio of the intel radius beyond
-#     the last update position
-    radius_movement_ratio = 0xE4F6EC # 0.25 from 0.33
-#   * a certain number of ticks have passed since the last update
-    tick_update_interval = 5 # from 30
 
+# make intelligence update every tick
 
-# We'll leave this alone for now.
-#.section h0; .set h0,0x76F21A #Moho::CIntelPosHandle::UpdatePos+0x3A
-#   fmul dword ptr [radius_movement_ratio]
-
-.section h1; .set h1,0x76F258 #Moho::CIntelPosHandle::UpdatePos+0x78
-    cmp edx, tick_update_interval
-
+.section h0; .set h0,0x76F1E0 #Moho::CIntelPosHandle::UpdatePos
+# void (__usercall*)(this@<edx>, int curTick@<eax>, Wm3::Vector3 *newPos@<ecx>)
+    push    ecx
+    push    ebx
+    push    esi
+    mov     esi, edx
+    cmp     byte ptr [esi+0x18], 0
+    mov     ebx, ecx
+    jz      short CLEANUP
+    mov     [esi+0x14], eax
+    call    0x76EFC0 #void __usercall Moho::CIntelPosHandle::Update(this@<esi>, Wm3::Vector3 *pos@<ebx>)
+CLEANUP:
+    pop     esi
+    pop     ebx
+    pop     ecx
+    ret
 )");
