66774b5f93aab4614b0beaf36b5be699f0bb19ed
4z0t <gastec139@gmail.com>
2024-09-18 15:48:39 +0300

    specify that edx changes

--- section/UserUnit/UserUnit.cpp
+++ section/UserUnit/UserUnit.cpp
@@ -1,13 +1,13 @@
 
 #include "UserUnit.h"
 
-int get_session_user_entities(Moho::BaseVector<UserEntity*> *output, int a2,
-                         Moho::struct_session_res3 *a3) {
+int get_session_user_entities(Moho::BaseVector<UserEntity *> *output, int a2,
+                              Moho::struct_session_res3 *a3) {
   int __result;
   asm("push %[a3];"
       "call 0x00503F80;"
       : "=a"(__result)
       : [output] "a"(output), [a2] "c"(a2), [a3] "g"(a3)
-      :);
+      : "edx");
   return __result;
 }
