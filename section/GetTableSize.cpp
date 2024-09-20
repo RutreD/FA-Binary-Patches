f72e3e910ffc7ecd29573a23fbe577b3d4a51956
Alexander <84857900+4z0t@users.noreply.github.com>
2024-09-20 22:30:30 +0300

    add table.unpack (#82)
    adds table.unpack that accepts indexes

--- section/GetTableSize.cpp
+++ section/GetTableSize.cpp
@@ -104,22 +104,39 @@ int TableClone(lua_State* L)
     return 1;
 }
 
-int RegTableFuncsDesc[] = {"getsize2",&GetTableSize,
-                           "empty2",&IsTableEmpty,
-                           "getn2",0x00927C20,
-                           "clone", &TableClone,
-                           0,0};
+// UI_Lua reprsl({table.unpack2({1,2,3,4},2,3)})
+// UI_Lua reprsl({table.unpack2({1,2,3,4},2)})
+// UI_Lua reprsl({table.unpack2({1,2,3,4})})
+// UI_Lua reprsl({table.unpack2({1,2,3,4},-1)})
+// UI_Lua reprsl({table.unpack2({1,2,3,4},-1000)})
+// UI_Lua reprsl({table.unpack2({1,2,3,4},0, 1000000)}) //stack overflow
 
-void RegTableFuncs()
-{
-    asm(
-        "CALL 0x0090DE00;"
-        "MOV DWORD PTR [ESP+0x8],%[RegTableFuncsDesc];"
-        "MOV DWORD PTR [ESP+0xC],0x0;"
-        "CALL 0x0090DE00;"
-        "JMP 0x009283B6;"
-        :
-        : [RegTableFuncsDesc] "i" (RegTableFuncsDesc)
-        :
-    );
+int lua_unpack(lua_State *l) {
+  luaL_checktype(l, 1, LUA_TTABLE);
+  const int n = lua_getn(l, 1);
+  const int start_i = luaL_optnumber(l, 2, 1);
+  const int end_i = luaL_optnumber(l, 3, n);
+  if(start_i > end_i) return 0;
+
+  const int n_stack = end_i - start_i + 1;
+  luaL_checkstack(l, n_stack, "too many results to unpack");
+  for (int i = start_i; i <= end_i; i++) {
+    lua_rawgeti(l, 1, i);
+  }
+  return n_stack;
+}
+
+int RegTableFuncsDesc[] = {"getsize2",&GetTableSize, "empty2", &IsTableEmpty,
+                           "getn2",   0x00927C20,    "clone",  &TableClone,
+                           "unpack", &lua_unpack,   0,        0};
+
+void RegTableFuncs() {
+  asm("CALL 0x0090DE00;"
+      "MOV DWORD PTR [ESP+0x8],%[RegTableFuncsDesc];"
+      "MOV DWORD PTR [ESP+0xC],0x0;"
+      "CALL 0x0090DE00;"
+      "JMP 0x009283B6;"
+      :
+      : [RegTableFuncsDesc] "i"(RegTableFuncsDesc)
+      :);
 }
