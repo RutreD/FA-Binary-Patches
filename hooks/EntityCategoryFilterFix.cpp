2899310f3383fe8d8007b66bdd80401cd96a46fb
4z0t <gastec139@gmail.com>
2024-09-20 20:04:26 +0300

    User set table instead of insert

---
+++ hooks/EntityCategoryFilterFix.cpp
@@ -0,0 +1,11 @@
+
+//Replaces Insert with set table
+asm(
+   ".section h0; .set h0,0x008BA4A9;" // at EntityCategoryFilterOut
+    "call 0x009087A0;" // Call LuaObject::SetObject(int index, LuaObject& obj)
+   ".section h1; .set h1,0x008BA177;" // at EntityCategoryFilterDown
+    "call 0x009087A0;"// Call LuaObject::SetObject(int index, LuaObject& obj)
+);
+
+
+// UI_Lua local _ti = table.insert local i = 0  _G.table.insert = function(... )  LOG(i) i = i + 1 return  _ti(unpack(arg)) end SelectUnits(EntityCategoryFilterDown(categories.COMMAND, GetSelectedUnits() or {}))  _G.table.insert = _ti
