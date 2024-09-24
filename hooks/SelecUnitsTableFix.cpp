f8655216d12ea3e00199d13933808c3af30dbbb7
4z0t <gastec139@gmail.com>
2024-09-24 12:42:59 +0300

    Create SelecUnitsTableFix.cpp

---
+++ hooks/SelecUnitsTableFix.cpp
@@ -0,0 +1,19 @@
+#include "../asm.h"
+
+asm(
+    // nop AssignTable call
+    SECTION(0, 0x008BD9BF)
+    "nop;"
+    "nop;"
+    "nop;"
+    "nop;"
+    "nop;"
+    "nop;"
+    "nop;"
+    SECTION(1, 0x008BD9CE)
+    "nop;"
+    "nop;"
+    "nop;"
+    "nop;"
+    "nop;"
+);
