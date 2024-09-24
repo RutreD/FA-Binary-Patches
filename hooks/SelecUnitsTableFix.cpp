35bc9777cb160fed763454cd39c9a2664ec0c0ba
4z0t <gastec139@gmail.com>
2024-09-24 12:48:54 +0300

    correct fix

--- hooks/SelecUnitsTableFix.cpp
+++ hooks/SelecUnitsTableFix.cpp
@@ -1,7 +1,7 @@
 #include "../asm.h"
 
 asm(
-    // nop AssignTable call
+    // nop AssignNewTable call
     SECTION(0, 0x008BD9BF)
     "nop;"
     "nop;"
@@ -16,4 +16,25 @@ asm(
     "nop;"
     "nop;"
     "nop;"
+    // nop PushStack
+    SECTION(2, 0x008BDBA6)
+    "nop;"
+    "nop;"
+    "nop;"
+    "nop;"
+    "nop;"
+
+    "nop;"
+    "nop;"
+    "nop;"
+    "nop;"
+    "nop;"
+
+    "nop;"
+    "nop;"
+    "nop;"
+    "nop;"
+    "nop;"
+    SECTION(3, 0x008BDC18)
+    "mov eax, 0;"
 );
