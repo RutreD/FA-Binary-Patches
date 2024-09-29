5b95a2f64dbbbb9c386809ff27dbff2f1cfed902
4z0t <gastec139@gmail.com>
2024-09-30 00:12:43 +0300

    Update LuaObject_ToVector.cxx

--- section/Override/LuaObject_ToVector.cxx
+++ section/Override/LuaObject_ToVector.cxx
@@ -2,7 +2,7 @@
 #include "LuaAPI.h"
 #include "moho.h"
 
-SHARED __cdecl Vector3f *LuaObject_ToVector3f(LuaObject *tbl, Vector3f *v) {
+SHARED Vector3f *__cdecl LuaObject_ToVector3f(LuaObject *tbl, Vector3f *v) {
   v->x = tbl->GetNumber(1);
   v->y = tbl->GetNumber(2);
   v->z = tbl->GetNumber(3);
